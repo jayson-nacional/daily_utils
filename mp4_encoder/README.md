ac193888fd613b1523ab8759014f1a73efdb86d6
Jayson Nacional <jayson.nacional@proton.me>
2026-07-12 16:07:24 +0800

    initialize mp4_encoder

---
+++ mp4_encoder/README.md
@@ -0,0 +1,4 @@
+# Usage  
+1. Clone the repository  
+2. Make the script executable. ```chmod +x mp4_encoder.sh```
+3. (Optional) Copy to path to execute anywhere ```sudo mv mp4_encoder.sh /usr/local/bin/[your_script_alis]```
---
+++ mp4_encoder/mp4_encoder.sh
@@ -0,0 +1,20 @@
+mkdir -p "$(pwd)/compressed"
+
+for file in $(pwd)/*; do
+	echo "Processing $file"
+
+	if [ -f "$file" ]; then
+		ext="${file##*.}"
+        ext=$(echo "$ext" | tr '[:upper:]' '[:lower:]')
+
+		case "$ext" in
+            mp4|mov)
+                echo "Processing video: $file"
+				HandBrakeCLI -i "$file" -o "$(pwd)/compressed/$(basename ${file%.*})__.mp4" --preset="Fast 1080p30" -e x265 -q 22 -E av_aac --optimize --non-anamorphic
+                ;;
+            *)
+                echo "Skipping non-video file: $file"
+                ;;
+        esac
+	fi
+done
