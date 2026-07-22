291f27becb2084601a47d1242479ff841b4c03fc
Jayson Nacional <jayson.nacional@proton.me>
2026-07-22 21:52:44 +0800

    fix path mapping for windows mp4 encoder

--- mp4_encoder/mp4_encoder.ps1
+++ mp4_encoder/mp4_encoder.ps1
@@ -1,6 +1,11 @@
-$null = New-Item -Path (Join-Path $PSScriptRoot "encoded") -ItemType Directory -Force
-$null = New-Item -Path (Join-Path $PSScriptRoot "thumbnails") -ItemType Directory -Force
-$null = New-Item -Path (Join-Path $PSScriptRoot "compressed") -ItemType Directory -Force
+$currentWorkingDirectory = $ExecutionContext.SessionState.Path.CurrentLocation.Path
+$encodedTargetPath = Join-Path -Path $currentWorkingDirectory -ChildPath "encoded"
+$thumbnailsTargetPath = Join-Path -Path $currentWorkingDirectory -ChildPath "thumbnails"
+$compressedTargetPath = Join-Path -Path $currentWorkingDirectory -ChildPath "compressed"
+
+New-Item -ItemType Directory -Path $encodedTargetPath -Force | Out-Null
+New-Item -ItemType Directory -Path $thumbnailsTargetPath -Force | Out-Null
+New-Item -ItemType Directory -Path $compressedTargetPath -Force | Out-Null
 
 Get-ChildItem -Path . -File | ForEach-Object {
 	$fullPath = $_.FullName
@@ -12,17 +17,17 @@ Get-ChildItem -Path . -File | ForEach-Object {
 		{
 			# Encode to mp4 x265
 			Write-Host "Processing video: $fullPath"
-			HandBrakeCLI -i "$fullPath" -o "$($PSScriptRoot)/encoded/$($baseName).mp4" --preset="Fast 1080p30" -e x265 -E av_aac  -q 22 --optimize --non-anamorphic --keep-metadata
+			HandBrakeCLI -i "$fullPath" -o "$($encodedTargetPath)/$($baseName).mp4" --preset="Fast 1080p30" -e x265 -E av_aac  -q 22 --optimize --non-anamorphic --keep-metadata
 			
 			# Generate thumbnail
 			Write-Host "Processing thumbnail"
-			ffmpeg -ss 00:00:01 -i "$($PSScriptRoot)/encoded/$($baseName).mp4" -vframes 1 "$($PSScriptRoot)/thumbnails/$($baseName).jpg"
+			ffmpeg -ss 00:00:01 -i "$($encodedTargetPath)/$($baseName).mp4" -vframes 1 "$($thumbnailsTargetPath)/$($baseName).jpg"
 			
 			# Embed thumbnail
-			ffmpeg -y -i "$($PSScriptRoot)/encoded/$($baseName).mp4" -i "$($PSScriptRoot)/thumbnails/$($baseName).jpg" -map 0 -map 1 -c copy -disposition:v:1 attached_pic "$($PSScriptRoot)/compressed/$($baseName).mp4"
+			ffmpeg -y -i "$($encodedTargetPath)/$($baseName).mp4" -i "$($thumbnailsTargetPath)/$($baseName).jpg" -map 0 -map 1 -c copy -disposition:v:1 attached_pic "$($compressedTargetPath)/$($baseName).mp4"
 			
 			# Restore metadata
-			exiftool -tagsFromFile "$fullPath" -FileCreateDate -FileModifyDate "$($PSScriptRoot)/compressed/$($baseName).mp4"
+			exiftool -tagsFromFile "$fullPath" -FileCreateDate -FileModifyDate "$($compressedTargetPath)/$($baseName).mp4"
 		}
 		default
 		{
@@ -33,8 +38,8 @@ Get-ChildItem -Path . -File | ForEach-Object {
 
 # Clear temp files
 Write-Host "Removing temp files..."
-Remove-Item -Path (Join-Path $PSScriptRoot "encoded") -Recurse -Force
-Remove-Item -Path (Join-Path $PSScriptRoot "thumbnails") -Recurse -Force
+Remove-Item -Path $encodedTargetPath -Recurse -Force
+Remove-Item -Path $thumbnailsTargetPath -Recurse -Force
 Write-Host "Operation succeeded!"
 
 [console]::beep(2000, 3000)
